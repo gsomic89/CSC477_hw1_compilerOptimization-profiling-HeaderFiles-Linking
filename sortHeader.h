e16eb7785f35eb6114b88e09e91e7ff8f4c29c76
Goran Somic <somicgoran@gmail.com>
2020-08-05 14:32:47 -0500

    Add files via upload

---
+++ sortHeader.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+extern void bubbleSort(int arrayLen, int* array);
+extern void insertionSort(int arrayLen, int* array);
+extern void exchange(int* array, int i, int j);
